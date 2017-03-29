07c70f98696ea333d70d524f8139d30245cb242e
oyvindhermansen <oyvind@ludensreklame.no>
2017-03-29 14:10:30 +0200

    testing prettier

--- src/toggle.js
+++ src/toggle.js
@@ -1,4 +1,6 @@
 export default function toggle() {
-  console.log('HELLO');
-  return 'hello';
+  console.log(
+    "hello hahahahahahade d ed ed ed  ed edede d ed ed ed e ed ed ed de "
+  );
+  return "hello";
 }
